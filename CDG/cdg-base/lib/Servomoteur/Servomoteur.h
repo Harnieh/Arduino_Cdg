9e201dcf5ac03219cad174bc7d4baa9747d944bf
Harnieh <55489933+Harnieh@users.noreply.github.com>
2021-03-03 10:47:42 +0100

    Changement Servomoteur

--- CDG/cdg-base/lib/Servomoteur/Servomoteur.h
+++ CDG/cdg-base/lib/Servomoteur/Servomoteur.h
@@ -1,11 +1,10 @@
 #pragma once
+
 class Servomoteur
-
-
 {
     long position_pas;   //Position sur l'axe en pas (long ?_position_pas)
-    float position_mm=0;    //Position sur l'axe en mm (float ?_position_mm)
-    float facteurDeConversion=0;    //Facteur de conversion en mm par pas pour l'axe (float ?_facteur)
+    float position_mm;    //Position sur l'axe en mm (float ?_position_mm)
+    float facteurDeConversion;    //Facteur de conversion en mm par pas pour l'axe (float ?_facteur)
     long limiteMin;     //Limite minimale de l'axe en ?? (long ?_min)
     long limiteMax;     //Limite maximale de l'axe (long ?_max)
 
--- CDG/cdg-base/src/main.cpp
+++ CDG/cdg-base/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>      //inclusion des fonctions arduino pour platformio
 #include <AccelStepper.h> //librairie de pilotage de moteurs pas à pas
 #include <Hx711.h>        //librairie de la carte de mesure en pont de wheatstone Hx711 attention librairie perso base sur première librairie
-#include <string.h>
+#include <stdint.h>
 #include "Capteur.h"
 #include "Servomoteur.h"
 #include "Piece.h"
