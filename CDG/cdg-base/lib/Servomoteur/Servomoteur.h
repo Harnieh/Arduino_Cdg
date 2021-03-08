b76ef0d836838777aa1ddbf6e471db50a5e682d5
Harnieh <55489933+Harnieh@users.noreply.github.com>
2021-03-08 08:20:29 +0100

    correction

--- CDG/cdg-base/lib/Servomoteur/Servomoteur.h
+++ CDG/cdg-base/lib/Servomoteur/Servomoteur.h
@@ -24,7 +24,7 @@ class Servomoteur
     Servomoteur(uint8_t pinDuPas, uint8_t pinDeDirection, float facteur, int vitessemax, int acceleration);  //Constructeur par défaut
     
     void setup();
-    void run();
+    void run(AccelStepper);
 };
 
 #endif // SERVOMOTEUR_H
